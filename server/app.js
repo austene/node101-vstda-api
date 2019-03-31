8222d33df562365d3d98f47c584a8cd9ebd656cb
AE <austence@sbcglobal.net>
2019-03-30 17:10:35 -0700

    adds server.listen to index.js and updates app.js with mock data array, '/' response, '/api/TodoItems' response, and '/api/TodoItems/:number' response.

--- server/app.js
+++ server/app.js
@@ -4,5 +4,40 @@ const morgan = require('morgan');
 const app = express();
 
 // add your code here
+var myArray = [
+    {
+      todoItemId: 0,
+      name: 'an item',
+      priority: 3,
+      completed: false
+    },
+    {
+      todoItemId: 1,
+      name: 'another item',
+      priority: 2,
+      completed: false
+    },
+    {
+      todoItemId: 2,
+      name: 'a done item',
+      priority: 1,
+      completed: true
+    }
+];
+console.log(myArray);
+
+app.get('/', (req, res) => {
+    res.send('ok'); //?
+});
+
+app.get('/api/TodoItems', (req, res) => {
+    res.send(myArray);
+});
+
+app.get('/api/TodoItems/:number', (req, res) => {
+//    res.send(myArray[req.params.number]);
+    var number = req.params.number;
+    res.send(myArray[number]);
+});
 
 module.exports = app;
